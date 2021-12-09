ee5a9471ae86184950dc1580434d6ff50762aabd
David Beesley <David.Beesley@tpr.gov.uk>
2021-12-09 14:57:55 +0000

    tidy

--- AdventofCode/Program.cs
+++ AdventofCode/Program.cs
@@ -2,7 +2,7 @@
 Day2 day2 = new Day2();
 Day3 day3 = new Day3();
 
-Console.WriteLine("Advent Of Code");
+Console.WriteLine("Advent Of Code 2021 by Alex Beesley");
 Console.WriteLine(@"
 Please Select a day from the following options:
 Day 1: Sonar Sweep --------------------------------------------------------------------------------- 1
@@ -11,21 +11,20 @@ Day 3: Binary Diagnostic -------------------------------------------------------
 ");
 Console.Write(">>>  ");
 string? Selection = Console.ReadLine();
+Console.Clear();
+
 
 if (Selection == "1")
 {
-    Console.Clear();
     day1.SolarSweepMain();
 }
 
 if (Selection == "2")
 {
-    Console.Clear();
     day2.DiveMain();
 }
 
 if (Selection == "3")
 {
-    Console.Clear();
     day3.BinaryDiagnosticMain();
 }
